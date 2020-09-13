de28c9221a5a9a8ea3a74cf5ddf2abc142b6ebb0
Diogo Campos <diogocampos@outlook.com>
2020-09-13 14:33:29 -0300

    Refactoring: Turning class component into function component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,34 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class SearchBar extends React.Component {
-  state = { searchTerm: 'Car' };
+const SearchBar = ({ onFormSubmit }) => {
+  const [term, setTerm] = useState('');
 
-  onInputChange = (event) => {
-    this.setState({ searchTerm: event.target.value });
+  const onInputChange = (event) => {
+    setTerm(event.target.value);
   };
 
-  onFormSubmit = (event) => {
+  const onSubmit = (event) => {
     event.preventDefault();
-    this.props.onSearchTermSubmit(this.state.searchTerm);
-    //TODO: MAke sure we call callback from parent component
+    onFormSubmit(term);
   };
 
-  render() {
-    return (
-      <div className="search-bar ui segment">
-        <form onSubmit={this.onFormSubmit} className="ui form">
-          <div className="field">
-            <label>Video Search</label>
-            <input
-              type="text"
-              value={this.state.searchTerm}
-              onChange={this.onInputChange}
-            />
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="search-bar ui segment">
+      <form onSubmit={onSubmit} className="ui form">
+        <div className="field">
+          <label>Video Search</label>
+          <input type="text" value={term} onChange={onInputChange} />
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default SearchBar;
